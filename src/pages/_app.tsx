0a1ad03272ae0dd0ec15e4d2f6c4a154002cd1eb
César Guilherme Lana Nonato <cesar.nonato@icloud.com>
2021-02-26 16:44:19 -0300

    Projeto finalizado nlw4 react

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/global.css';
+
+import { ChallengesProvider } from '../contexts/ChanlangesContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default MyApp
